7dd66800385552bc59f026df4cbdb5847d00c7bf
DN-debug <75267055+DN-debug@users.noreply.github.com>
2021-04-22 15:24:32 +0530

    commit

--- install.sh
+++ install.sh
@@ -2,14 +2,16 @@
 
 PASSWD="$(zenity --password --title=Authentication)\n"
 
-if [ -e /etc/startpages/wow ];
+if [ -e /etc/startpages/wow/index.html ];
 then
     rm -rf wow
-    echo -e $PASSWD | sudo -A cp -r Peux-Startpages/*/ /etc/startpages/
-    rm -rf /tmp/fetcher/Peux-Startpages
+    echo -e $PASSWD | sudo -A cp -r */ /etc/startpages/
+    cd ../
+    rm -rf Peux-Startpages
 
 else
-    echo -e $PASSWD | sudo -A mkdir /etc/startpages && sudo cp -r Peux-Startpages/*/ /etc/startpages/
-    rm -rf /tmp/fetcher/Peux-Startpages
+    echo -e $PASSWD | sudo -A mkdir /etc/startpages && sudo cp -r */ /etc/startpages/
+    cd ../
+    rm -rf Peux-Startpages
 fi
 
