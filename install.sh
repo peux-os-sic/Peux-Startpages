3324cf460fe0b97fe01b993187d0b4c917b6e51b
DN-debug <75267055+DN-debug@users.noreply.github.com>
2021-04-29 17:22:24 +0530

    commit

--- install.sh
+++ install.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
+###########################################################
+# Author - DN-debug
+# Description - Peux OS startpages' copy script
+###########################################################
+
 PASSWD="$(zenity --password --title=Authentication)\n"
 
-if [ -e /etc/startpages/wow/index.html ];
-then
-    echo "running if"
-    cd ../
-    rm -rf Peux-Startpages/wow
-    echo -e $PASSWD | sudo -S cp -r Peux-Startpages/*/ /etc/startpages/
-    rm -rf Peux-Startpages
+if zenity --question --text="Want to proceed?"
+then 
+    PASSWD="$(zenity --password --title=Authentication)\n"
+
+    if [ -e /etc/startpages/wow/index.html ];
+    then
+        echo "running if"
+        cd ../
+        rm -rf Peux-Startpages/wow
+        echo -e $PASSWD | sudo -S cp -r Peux-Startpages/*/ /etc/startpages/
+    
+    else
+        echo "running else"
+        cd ../
+        echo -e $PASSWD | sudo -S mkdir /etc/startpages && sudo cp -r Peux-Startpages/*/ /etc/startpages/
+    fi
 
 else
-    echo "running else"
-    cd ../
-    echo -e $PASSWD | sudo -S mkdir /etc/startpages && sudo cp -r Peux-Startpages/*/ /etc/startpages/
-    rm -rf Peux-Startpages
+    notify-send "Operation skipped!"
 fi
 
+rm -rf /tmp/fetcher/Peux-Startpages
+notify-send "Done!"
